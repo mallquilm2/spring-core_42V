4f2ccb68787777fa2aa03d4a4e443ccbbdd84de8
miguel <mallquilm@gmail.com>
2026-02-18 22:15:42 -0500

    lab03 ciclo de vida

--- src/main/java/edu/cibertec/beans/DocumentoExcel.java
+++ src/main/java/edu/cibertec/beans/DocumentoExcel.java
@@ -2,6 +2,14 @@ package edu.cibertec.beans;
 
 public class DocumentoExcel implements Imprimible{
 
+    private void iniciar(){
+        System.out.println("Iniciando la creación de la instancia del bean");
+    }
+
+    private void destruir(){
+        System.out.println("Destruyendo instancia creada");
+    }
+
     @Override
     public String imprimir() {
         return "Imprimiendo desde un archivo excel.";
